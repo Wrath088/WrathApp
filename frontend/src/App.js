9f2c4f7edf5cc92781f2f0d695003d02763b9786
Wrath <95567213+Wrath2034@users.noreply.github.com>
2022-11-29 16:44:37 -0600

    first commit

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import LandingPage0 from "./scenes/pages/landingPages/LandingPage0";
+
+function App() {
+  return (
+    <>
+      <LandingPage0 />
+    </>
+  );
+}
+
+export default App;
---
+++ frontend/src/scenes/pages/landingPages/LandingPage0.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import logo from "../../../utils/assets/logos/STS/medium.png";
+
+const LandingPage0 = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1 className="App-link">Welcome</h1>
+        <p>We are Starting Over</p>
+        {/* <a
+          className="App-link"
+          // href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Login
+        </a> */}
+      </header>
+    </div>
+  );
+};
+
+export default LandingPage0;
